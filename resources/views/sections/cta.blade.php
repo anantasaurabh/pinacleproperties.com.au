0c3a5f9725398c856a371a587164880e06fa0957
ASA <web@vps-268290d1.vps.ovh.ca>
2026-05-16 16:30:21 +0000

    updared form and layouts

--- resources/views/sections/cta.blade.php
+++ resources/views/sections/cta.blade.php
@@ -3,6 +3,9 @@
         <h2>Ready to Connect?</h2>
         <p>Take the next step in your property journey with Pinnacle Home and Investment.</p>
         <div class="cta-btns">
+            <a href="tel:1300000000" class="btn-cta">
+                <i class="fas fa-phone-alt"></i> Talk to Expert
+            </a>
             <a href="#inquiry" class="btn-cta">
                 <i class="fas fa-home"></i> Secure Your Dream Home
             </a>
